4361f990289d65cc0b181d1a9a5b7d9fbb64b24e
Sven Trittler <45872415+trittsv@users.noreply.github.com>
2025-04-18 12:37:28 +0200

    Update Condition.hpp

--- src/include/fastdds_modern_cpp_api/dds/core/cond/Condition.hpp
+++ src/include/fastdds_modern_cpp_api/dds/core/cond/Condition.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fastdds/dds/core/condition/Condition.hpp>
+#include <fastdds/dds/core/condition/GuardCondition.hpp>
 
 namespace fastdds_modern_cpp_api {
 namespace dds {
@@ -9,7 +10,20 @@ namespace cond {
 
 
 //using Condition = eprosima::fastdds::dds::Condition;
+class GuardCondition {
+public:
+    GuardCondition() {
 
+    }
+
+    void trigger_value(bool value) {
+        m_guardCondition.set_trigger_value(value);
+    }
+
+
+    eprosima::fastdds::dds::GuardCondition m_guardCondition;
+
+};
 
 }
 }
